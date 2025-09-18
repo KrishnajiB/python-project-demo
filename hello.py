25d79d0a5226fbb9bcb2eb579b64b26a6988734b
Krishnaji Biradar <krishnabiradar2008@gmail.com>
2025-09-18 09:40:10 +0530

    Spelling changed

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 def greet():
     print("Hello, from local machine!")
-    print("This new hello from local machine, as well from ne branch 'feature/new-feature")
+    print("This new hello from local machine, as well from new branch 'feature/new-feature")
 
 greet()
